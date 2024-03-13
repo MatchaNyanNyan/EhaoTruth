c56a9f521ab7d082a94197fc866aedebdbac921e
MatchaNyanNyan <153619669+MatchaNyanNyan@users.noreply.github.com>
2024-03-13 12:24:36 +0700

    Implemented Library
    Hooked up links to writings

--- script.js
+++ script.js
@@ -6,20 +6,17 @@ tabs.forEach(tab => {
     const targetTab = document.querySelector(`[data-tab="${targetId}"]`);
 
     if (targetTab) {
-      // Remove .active class from all tabs
+
       tabs.forEach(tab => {
         tab.classList.remove("active");
       });
 
-      // Add .active class to the clicked tab
       tab.classList.add("active");
 
-      // Hide all tab_content elements
       document.querySelectorAll(".tab_content").forEach(content => {
         content.style.display = "none";
       });
 
-      // Display the target tab content
       targetTab.style.display = "block";
       targetTab.classList.add("custom-tab-content");
     }
